747b28fb2d2047f2e274f0d3eaa2ddd724f98b34
Dario Pacino <dpacino@gmail.com>
2020-03-10 21:49:32 +0100

    Fixed bug with the integer parameters

--- src/AlgoTuner.jl
+++ src/AlgoTuner.jl
@@ -2,54 +2,64 @@ module AlgoTuner
 
 import Random
 using Random
+using Pkg
 # INTERFACE RELATED TYPES AND FUNCTION
 
 rng = MersenneTwister(1234)
 
-@enum ParamType IntParam FloatParam
 @enum TunerVerbosity Silent IncumbentOnly ShowAll ShowDebug
 
-struct FuncParam
-    pType::ParamType
+struct FuncParam{T<:Number}
     name::String
-    LB::Float64
-    UB::Float64
-    #values::Array{Float64,1}
-    #FuncParam(pType,name,LB,UB) = new FuncParam(pType,name,LB,UB,[])
-    #FuncParam(pType,name,values) = new FuncParam(pType,name,values[1],values[end],values)
+    LB::T
+    UB::T
 end
 
 mutable struct FuncCommand
     cmd::Function
     params::Array{FuncParam,1}
-    param_init_vals::Array{Float64,1}
+    param_init_vals::Array{Number,1}
 end
 
 function createRuntimeCommand(func::Function)
     return FuncCommand(func,[],[])
 end
 
-function addIntParam(func::FuncCommand, name::String, LB, UB)
+function addParam(::Type{T},func::FuncCommand, name::String, LB::T, UB::T) where {T<:Number}
     if LB >= UB
         error("LB should be smaller than UB")
     end
-    push!(func.params, FuncParam(IntParam,name,LB,UB))
-    push!(func.param_init_vals, rand(rng,LB:UB))
+    push!(func.params, FuncParam(name,LB,UB))
+    if typeof(LB)<:Integer
+        push!(func.param_init_vals, rand(rng,LB:UB))
+    else
+        push!(func.param_init_vals, LB+rand(rng)*(UB-LB))
+    end
 end
 
-function addFloatParam(func::FuncCommand, name::String, LB, UB)
-    if LB >= UB
-        error("LB should be smaller than UB")
-    end
-    push!(func.params, FuncParam(FloatParam,name,LB,UB))
-    push!(func.param_init_vals, LB+rand(rng)*(UB-LB))
+addParam(func::FuncCommand, name::String, LB::Float64, UB::Float64) = addParam(Float64,func, name, LB, UB)
+
+
+function addIntParam(func::FuncCommand, name::String, LB::Integer, UB::Integer)
+    addParam(Int64,func,name,LB,UB)
+end
+
+function addFloatParam(func::FuncCommand, name::String, LB::Number, UB::Number)
+        addParam(Float64,func,name,convert(Float64,LB),convert(Float64,UB))
 end
 
 function addInitialValues(func::FuncCommand, values)
     if length(values)!=length(func.params)
         error("The number of initialized parameters should be $(length(func.params))")
     end
-    func.param_init_vals = deepcopy(values)
+    #func.param_init_vals=deepcopy(values)
+    for i in 1:length(func.params)
+        if typeof(func.params[i].LB)<:Integer
+            func.param_init_vals[i]=convert(Int64,values[i])
+        else
+            func.param_init_vals[i]=convert(Float64,values[i])
+        end
+    end
 end
 
 function runCommand(func::FuncCommand, seed, instance, values)
@@ -77,13 +87,13 @@ function randomMoveOperator(func::FuncCommand, parVals)
     param = func.params[p]
     if rand(rng)>=0.5 #increase value
         if param.pType == IntParam
-            return (p,trunc(Int64,rand(rng,parVals[p]:param.UB)))
+            return (p,rand(rng,parVals[p]:trunc(Int64,param.UB)))
         else
             return (p,parVals[p]+rand(rng)*(param.UB-parVals[p]))
         end
     else # descrease value
         if param.pType == IntParam
-            v =trunc(Int64,rand(rng,param.LB:parVals[p]))
+            v =rand(rng,trunc(Int64,param.LB):parVals[p])
             #println(param.LB," ",parVals[p], " -> ",v)
             return (p,v)
         else
@@ -175,7 +185,7 @@ function tune(func::FuncCommand, instances::Array{String,1},
 
     #ver="0.1.3"
     logText(elapsed_time,"----------------------------------------------------")
-    logText(elapsed_time,"                AlgoTuner")
+    logText(elapsed_time,"                AlgoTuner ver. ",version())
     logText(elapsed_time,"----------------------------------------------------")
     logText(elapsed_time," Verbosity level: $(verbosity)")
     logText(elapsed_time," Time limit: $(timeLimit)")
@@ -222,5 +232,7 @@ function tune(func::FuncCommand, instances::Array{String,1},
     end
     logBestIncumbent(elapsed_time,func,bestCost,bestParValues, verbosity)
 end
+
+version() = VersionNumber(Pkg.TOML.parsefile(joinpath(@__DIR__, "..", "Project.toml"))["version"])
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,36 @@
 using Test
 using AlgoTuner
+using Pkg
 
-@testset "Execution of client algorithm" begin
-    testF(inst,a,b,c) = a+b*c
-    cmd = AlgoTuner.createRuntimeCommand(testF)
-    AlgoTuner.addIntParam(cmd,"a",1,3)
-    AlgoTuner.addIntParam(cmd,"b",1,3)
-    AlgoTuner.addFloatParam(cmd,"c",1,3)
+@testset "AlgoTuner tests" begin
+    @testset "Parameter types" begin
+        function testF(seed,inst,a,b,c)
+            println("************* ",AlgoTuner.version())
+            @test typeof(a)==Int64
+            @test typeof(b)==Int64
+            @test typeof(c)==Float64
+        end
+        cmd = AlgoTuner.createRuntimeCommand(testF)
+        AlgoTuner.addIntParam(cmd,"a",1,3)
+        AlgoTuner.addIntParam(cmd,"b",1,3)
+        AlgoTuner.addFloatParam(cmd,"c",1,3)
+        AlgoTuner.addInitialValues(cmd,[1.0,3,3.1])
 
-    @test AlgoTuner.runCommand(cmd,"test",[1,2,3]) == 7
-    @test AlgoTuner.runCommand(cmd,"test",[4,2,2]) == 8
+        #parValues = deepcopy(cmd.param_init_vals)
+        #AlgoTuner.execute(cmd,["test"],parValues,1,[123])
+        AlgoTuner.runCommand(cmd,123,"test",cmd.param_init_vals)
+
+    end
+
+    @testset "Execution of client algorithm" begin
+        testF(seed,inst,a,b,c) = a+b*c
+        cmd = AlgoTuner.createRuntimeCommand(testF)
+        AlgoTuner.addIntParam(cmd,"a",1,3)
+        AlgoTuner.addIntParam(cmd,"b",1,3)
+        AlgoTuner.addFloatParam(cmd,"c",1,3)
+
+        @test AlgoTuner.runCommand(cmd,123,"test",[1,2,3]) == 7
+        @test AlgoTuner.runCommand(cmd,123,"test",[4,2,2]) == 8
+
+    end
 end
